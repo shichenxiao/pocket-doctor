c9658b114e371d19b95a48ce1e482d0296d39337
shichenxiao <1739118128@qq.com>
2018-06-26 17:10:42 +0800

    delete some

--- backend/videoBackEnd-master/src/router/index.js
+++ backend/videoBackEnd-master/src/router/index.js
@@ -6,27 +6,17 @@ Vue.use(Router)
 import Login              from '../components/Login.vue'
 import Admin              from '../components/Admin.vue'
 import newslist         from '../components/newslist.vue'
-import Userinfo           from '../components/Userinfo.vue'
+
 import Carousel           from '../components/Carousel.vue'
-import CarouselEdit       from '../components/CarouselEdit.vue'
+
 import MovieEdit          from '../components/MovieEdit.vue'
 import CarouselUpload     from '../components/CarouselUpload.vue'
 import testlist        from '../components/testlist.vue'
 import knowledge         from '../components/knowledge.vue'
-import FoundTopicAdd      from '../components/FoundTopicAdd.vue'
-import FoundTopicList     from '../components/FoundTopicList.vue'
-import MovieCategoryList  from '../components/MovieCategoryList.vue'
-import MovieCategoryAdd   from '../components/MovieCategoryAdd.vue'
+
 import MainBoard          from '../components/MainBoard.vue'
-import FoundTopicEdit     from '../components/FoundTopicEdit.vue'
-import MovieCategoryEdit  from '../components/MovieCategoryEdit.vue'
-import UserinfoEdit       from '../components/UserinfoEdit.vue'
-import TopicCategoryAdd   from '../components/TopicCategoryAdd.vue'
-import TopicCategoryEdit  from '../components/TopicCategoryEdit.vue'
-import TopicCategory      from '../components/TopicCategory.vue'
-import TopicCommentlist   from '../components/TopicCommentlist.vue'
 import videolist   from '../components/videolist.vue'
-import FeedBack           from '../components/FeedBack.vue'
+
 import knowledgecomment           from '../components/knowledgecomment.vue'
 import newscomment           from '../components/newscomment.vue'
 import vediocomment           from '../components/vediocomment.vue'
@@ -50,9 +40,7 @@ export default new Router({
       },
       children: [
         {path: '/newslist',component:resolve=>System.import('../components/newslist'),name:'newslist'}, //路由懒加载
-        {path: '/userinfo',component:resolve=>System.import('../components/Userinfo'),name:'userinfo'},
         {path: '/carousel',component:resolve=>System.import('../components/Carousel'),name:'carousel'},
-        {path: '/carouseledit',component:resolve=>System.import('../components/CarouselEdit'),name:'carouselEdit'},
         {path: '/movieedit',component: MovieEdit,name:'movieEdit'},
         {path: '/carouselupload',component:CarouselUpload,name:'CarouselUpload'},
         {path: '/knowledgecomment',component:knowledgecomment,name:'knowledgecomment'},
@@ -65,20 +53,13 @@ export default new Router({
         {path: '/knowledge',component:knowledge,name:'knowledge'},
         {path: '/upknowledge',component:upknowledge,name:'upknowledge'},
         {path: '/changecontent',component:changecontent,name:'changecontent'},
-        {path: '/foundTopicAdd',component:FoundTopicAdd,name:'FoundTopicAdd'},
-        {path: '/foundTopicList',component:FoundTopicList,name:'FoundTopicList'},
-        {path: '/movieCategoryAdd',component:MovieCategoryAdd,name:'MovieCategoryAdd'},
-        {path: '/movieCategoryList',component:MovieCategoryList,name:'MovieCategoryList'},
+
+
+
         {path: '/mainBoard',component:MainBoard,name:'MainBoard'},
-        {path: '/foundTopicEdit',component:resolve => System.import('../components/FoundTopicEdit'),name:'FoundTopicEdit'},
-        {path: '/movieCategoryEdit',component:MovieCategoryEdit,name:'MovieCategoryEdit'},
-        {path: '/userinfoEdit',component:UserinfoEdit,name:'userinfoEdit'},
-        {path: '/topicCategory',component:TopicCategory, name:'TopicCategory'},
-        {path: '/topicCategoryAdd',component:TopicCategoryAdd,name:'TopCategoryAdd'},
-        {path: '/topicCategoryEdit',component:TopicCategoryEdit,name:'TopicCategoryEdit'},
+
         {path: '/videolist',component:videolist,name:'videolist'},
-        {path: '/topicCommentlist',component:TopicCommentlist,name:'TopicCommentlist'}, 
-        {path: '/FeedBack',component:FeedBack ,name:'FeedBack'}
+
       ]
     }
   ]
